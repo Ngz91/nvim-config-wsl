baaa9fb3220cd903e4ec87b87c66ccef16e9422d
Nevio <neviogomez91@gmail.com>
2022-07-24 23:05:50 -0400

    Added settings for tsserver

---
+++ lua/user/lsp/settings/tsserver.lua
@@ -0,0 +1,6 @@
+local opts = {
+  filetypes = {"javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx"},
+  root_dir = function () return vim.loop.cwd() end
+}
+
+return opts
