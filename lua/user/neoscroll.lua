f4919808a11538232491c84bdfa7a25c461d5823
Nevio <neviogomez91@gmail.com>
2022-08-14 17:13:29 -0400

    Changed some settings

--- lua/user/neoscroll.lua
+++ lua/user/neoscroll.lua
@@ -10,7 +10,7 @@ neoscroll.setup {
   hide_cursor = true, -- Hide cursor while scrolling
   stop_eof = true, -- Stop at <EOF> when scrolling downwards
   respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-  cursor_scrolls_alone = false, -- The cursor will keep on scrolling even if the window cannot scroll further
+  cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
   use_local_scrolloff = false, --Use local scope of scrolloff
   --easing_function = nil, -- Default easing function
   --pre_hook = nil, -- Function to run before the scrolling animation starts
